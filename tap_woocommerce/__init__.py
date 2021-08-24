9b7c5f8649a1d80a0a27ab8edb9c0bdc77ad6406
Manoj <saimanoj58@gmail.com>
2021-08-24 11:53:11 +0530

    added repeat/first time customer logic

--- tap_woocommerce/__init__.py
+++ tap_woocommerce/__init__.py
@@ -31,7 +31,8 @@ CONFIG = {
 
 ENDPOINTS = {
     "orders":"/wp-json/wc/v3/orders?after={0}&orderby=date&order=asc&per_page=100&page={1}&consumer_key={2}&consumer_secret={3}",
-    "reports":"/wp-json/wc/v3/reports/sales?date_min={0}&date_max={1}&consumer_key={2}&consumer_secret={3}"
+    "reports":"/wp-json/wc/v3/reports/sales?date_min={0}&date_max={1}&consumer_key={2}&consumer_secret={3}",
+    "customer":"/wp-json/wc/v3/orders?customer_id={0}&consumer_key={1}&consumer_secret={2}"
 }
 
 def get_endpoint(endpoint, kwargs):
@@ -45,6 +46,12 @@ def get_endpoint(endpoint, kwargs):
         consumer_secret=unidecode(kwargs[3]).strip()
 
         return CONFIG["url"]+ENDPOINTS[endpoint].format(after,page,consumer_key,consumer_secret)
+    elif endpoint == "customer":
+        customer_id = kwargs[0]
+        consumer_key=unidecode(kwargs[1]).strip()
+        consumer_secret=unidecode(kwargs[2]).strip()
+
+        return CONFIG["url"]+ENDPOINTS[endpoint].format(customer_id,consumer_key,consumer_secret)
     else:
         date_min = unidecode(kwargs[0])
         date_max = unidecode(kwargs[1])
@@ -139,7 +146,12 @@ def filter_order(order):
         shipping_lines = [filter_shipping(ship) for ship in order["shipping_lines"]]
     else:
         shipping_lines = None
- 
+    if "customer_id" in order and order["customer_id"] and order["customer_id"] != 0:
+        endpoint = get_endpoint("cusotmer", [order["customer_id"], CONFIG["consumer_key"], CONFIG["consumer_secret"]])
+        customer = gen_request("customer",endpoint)
+        new_customer = False if len(customer) > 1 else True
+    else:
+        new_customer = None
     try:
         filtered = {
             "number": str(order["number"]),
@@ -168,6 +180,7 @@ def filter_order(order):
             "payment_method": str(order["payment_method"]),
             "payment_method_title": str(order["payment_method_title"]),
             "transaction_id": str(order["transaction_id"]),
+            "new_customer": new_customer,
         
             "shipping_lines": order["shipping_lines"],
             "line_items":line_items,
