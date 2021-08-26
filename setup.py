6e54a42256ad98d2831595f774db57acdfd7cc3e
Manoj <saimanoj58@gmail.com>
2021-08-26 15:39:44 +0530

    fixed date issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-woocommerce-v3",
-    version="0.1.1",
+    version="0.1.3",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
